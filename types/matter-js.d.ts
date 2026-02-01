d512fdad7152512be6c4df2ccf1b7ba94935a230
md-sahil-alam <mdsahil.prof@gmail.com>
2026-02-01 17:22:40 +0530

    fix

---
+++ types/matter-js.d.ts
@@ -0,0 +1,10 @@
+declare module 'matter-js' {
+  export const Engine: any;
+  export const World: any;
+  export const Bodies: any;
+  export const Body: any;
+  export const Events: any;
+  export const Render: any;
+  export const Runner: any;
+  export default any;
+}
